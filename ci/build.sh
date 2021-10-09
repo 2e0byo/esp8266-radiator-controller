05fe5f81a666e827669cf98d1f08e90b66b332f4
John Maximilian <2e0byo@gmail.com>
2021-10-09 20:03:09 +0100

    ci: try again

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,7 @@ set +x
 echo "Building"
 cd firmware/micropython
 
+PWD=$(pwd)
 DOCKER="docker run --rm -v $HOME:$HOME -u $UID -w $PWD larsks/esp-open-sdk"
 NPROC=$(nproc)
 
@@ -12,7 +13,7 @@ $DOCKER make -C mpy-cross -j $NPROC
 $DOCKER make submodules -C ports/esp8266 -j $NPROC
 $DOCKER make -C ports/esp8266 -j $NPROC
 
-FIRMWARE=ports/esp8266/build-GENERIC/firmware-combined.elf
+FIRMWARE=ports/esp8266/build-GENERIC/firmware-combined.bin
 mv $FIRMWARE ../
 echo "Build firmware is at firmware/$FIRMWARE"
 cd ../../
