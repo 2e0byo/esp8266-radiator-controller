57cb43e10d1d1aff56f354c7f9f0dde7e31e5829
John Maximilian <2e0byo@gmail.com>
2021-10-10 20:51:50 +0100

    feat(hal): finish hal

--- src/hal.py
+++ src/hal.py
@@ -16,7 +16,7 @@ class Radiator:
         self._relay = relay
         relay.off()
         self._warm_ms = 30 * 60 * 1000
-        self._timer = Delay_ms(self._warm_off, self.delay)
+        self._timer = Delay_ms(self._stop_warm, self.delay)
         self._timer.stop()
         self._warming = False
         self._heating = False
@@ -61,7 +61,7 @@ class Radiator:
                 self._timer.stop()
             self._update.set(state)
 
-    def stop_warm(self):
+    def _stop_warm(self):
         self._update.set(self.WARMING_OFF)
 
     async def _loop(self):
@@ -77,44 +77,137 @@ class Radiator:
                 self.relay_off()
 
 
-class LED:
+class Led:
     # led which can be flashed nicely
     RED = 0
     GREEN = 1
+    FADE_STEPS = bytearray(round(sin(pi * x / 100) * 255) for x in range(100))
 
     def __init__(self, led, other_pin):
         self.other_pin = other_pin
         self.led = led
+        self._brightness = 0
         self.set_colour(self.GREEN)
-        self.set_brightness(0)
 
     def set_colour(self, col):
+        self._colour = col
         if col is self.GREEN:
             self.other_pin.off()
         else:
             self.other_pin.on()
+        self.set_brightness(self._brightness)
 
     def set_brightness(self, duty):
-        if col is self.GREEN:
+        self._brightness = duty
+        if self._colour is self.GREEN:
             self.led.duty(duty)
         else:
             self.led.duty(1023 - duty)
 
-    async def flash(self, colour, duration):
+    async def flash(self, colour=None, duration=1):
         gap = duration / 100
-        self.set_colour(colour)
+        if colour is not None:
+            self.set_colour(colour)
         for step in range(100):
-            self.set_brightness(round(sin(pi * step / 100) * 1023))
+            self.set_brightness(self.FADE_STEPS[step] * 4)
             await asyncio.sleep(gap)
         self.set_brightness(0)
 
 
-def init(loop):
-    _relay = Pin(0, Pin.OUT)
-    radiator = Radiator(_relay)
-    radiator.start(loop)
+class Buzzer:
+    SOUND_OFF = 0
+    SOUND = 1
+    RING = 2
+    RING_OFF = 3
 
-    _led1 = Pin(16, Pin.OUT)
-    _led2 = Pin(5, Pin.OUT)
-    _pwm = PWM(led2)
-    led = LED(_pwm, _led1)
+    def __init__(self, buzzer):
+        self._buzzer = buzzer
+        self._buzzer.duty(0)
+        self._sounding = False
+        self._ringing = False
+        self._update = Message()
+        self.ring_freq = 600
+
+    def start(self, loop):
+        loop.create_task(self._loop())
+
+    @property
+    def sounding(self):
+        return self._sounding
+
+    @sounding.setter
+    def sounding(self, val):
+        if val and not self._sounding:
+            self._update.set(self.SOUND)
+        elif not val and self._sounding:
+            self._update.set(self.SOUND_OFF)
+
+    @property
+    def ringing(self):
+        return self._ringing
+
+    @ringing.setter
+    def ringing(self, val):
+        if val and not self._ringing:
+            self._update.set(self.RING)
+        elif not val and self._ringing:
+            self._update.set(self.RING_OFF)
+
+    async def _siren(self):
+        self._buzzer.duty(512)
+        while self._sounding:
+            self._buzzer.freq(440)
+            await asyncio.sleep(0.5)
+            self._buzzer.freq(600)
+            await asyncio.sleep(0.5)
+        self._buzzer.duty(0)
+
+    async def _ring(self):
+        while self._ringing:
+            for _ in range(2):
+                self._buzzer.freq(self.ring_freq)
+                self._buzzer.duty(512)
+                await asyncio.sleep(0.1)
+                self._buzzer.duty(0)
+                await asyncio.sleep(0.1)
+            await asyncio.sleep(0.4)
+        self._buzzer.duty(0)
+
+    async def beep(self, freq=600, duration_ms=500):
+        self._buzzer.duty(0)
+        self._buzzer.freq(freq)
+        self._buzzer.duty(512)
+        await asyncio.sleep_ms(duration_ms)
+        self._buzzer.duty(0)
+
+    def _loop(self):
+        while True:
+            msg = await self._update.wait()
+            if msg is self.SOUND and not self._sounding:
+                self._sounding = True
+                asyncio.get_event_loop().create_task(self._siren())
+            elif msg is self.SOUND_OFF and self._sounding:
+                self._sounding = False
+            elif msg is self.RING and not self._ringing:
+                self._ringing = True
+                asyncio.get_event_loop().create_task(self._ring())
+            elif msg is self.RING_OFF and self._ringing:
+                self._ringing = False
+
+
+_relay = Pin(0, Pin.OUT)
+radiator = Radiator(_relay)
+
+_led1 = Pin(16, Pin.OUT)
+_led2 = Pin(5, Pin.OUT)
+
+_pwm = PWM(_led2)
+led = Led(_pwm, _led1)
+
+_buzzer = PWM(Pin(12))
+buzzer = Buzzer(_buzzer)
+
+
+def init(loop):
+    radiator.start(loop)
+    buzzer.start(loop)
