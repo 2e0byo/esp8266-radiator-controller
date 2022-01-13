17f7065eecb6d498814decd1df15ae2c46157142
John Maximilian <2e0byo@gmail.com>
2022-01-13 19:09:05 +0000

    build: match lns as well.

--- .local-deploy.sh
+++ .local-deploy.sh
@@ -2,5 +2,5 @@
 
 cd src/
 
-FILES=$(find . -mindepth 1 -type f -name '*.py' |  awk '{printf "putc " $1 "; "}')
+FILES=$(find . -mindepth 1 -name '*.py' |  awk '{printf "putc " $1 "; "}')
 mpfshell -o ser:$1 -n -c $FILES
--- ci/setup.sh
+++ ci/setup.sh
@@ -9,7 +9,7 @@ MODULES=micropython/ports/esp8266/modules/
 find lib -maxdepth 1 -mindepth 1 -exec ln -rs {} $MODULES \;
 
 # setup internal dependencies
-find ../src -maxdepth 1 -mindepth 1 -type f -name '*.py' -exec ln -rs {} $MODULES \;
+find ../src -maxdepth 1 -mindepth 1 -name '*.py' -exec ln -rs {} $MODULES \;
 find ../src/ -maxdepth 1 -mindepth 1 -type d -exec ln -rs {} $MODULES \;
 cd ../
 
