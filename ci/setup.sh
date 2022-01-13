86fb59188457f227687e836b4bf7b28a1b4449b7
John Maximilian <2e0byo@gmail.com>
2022-01-13 17:33:37 +0000

    ci: fix setup script.

--- ci/setup.sh
+++ ci/setup.sh
@@ -3,15 +3,14 @@ set -e
 set +x
 
 cd firmware
-# setup external dependencies
 MODULES=micropython/ports/esp8266/modules/
-ln -rs micropython-lib/python-stdlib/logging/logging.py $MODULES
-ln -rs tinyweb/tinyweb $MODULES
-ln -rs micropython-async/v3/primitives $MODULES
+
+# setup external dependencies
+find lib -maxdepth 1 -mindepth 1 -exec ln -rs {} $MODULES \;
 
 # setup internal dependencies
-find ../src -type f -name '*.py' -maxdepth 1 -exec ln -rs {} $MODULES \;
-find ../src -type d -maxdepth 1 -exec ln -rs {} $MODULES \;
+find ../src -maxdepth 1 -mindepth 1 -type f -name '*.py' -exec ln -rs {} $MODULES \;
+find ../src/ -maxdepth 1 -mindepth 1 -type d -exec ln -rs {} $MODULES \;
 cd ../
 
 echo "Done setting up"
