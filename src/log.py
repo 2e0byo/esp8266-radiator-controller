54f8275cd154dfc41564df9fcd1ef82835d5e1b2
John Maximilian <2e0byo@gmail.com>
2022-01-13 19:43:51 +0000

    feat: log.py

---
+++ src/log.py
@@ -0,0 +1,28 @@
+import logging
+from packing.text import RotatingLog
+from settings import settings
+
+rotating_log = RotatingLog(
+    "syslog",
+    "static/",
+    log_lines=settings.get("syslog_lines", 200),
+    keep_logs=2,
+    timestamp=True,
+)
+
+
+class RotatingLogHandler(logging.Handler):
+    def __init__(self, log):
+        super().__init__()
+        self.log = log
+
+    def emit(self, record):
+        if record.levelno >= self.level:
+            self.log.append(self.formatter.format(record))
+
+
+rotating_handler = RotatingLogHandler(rotating_log)
+rotating_handler.setLevel(logging.INFO)
+rotating_handler.setFormatter(
+    logging.Formatter("%(name)s - %(levelname)s: %(message)s")
+)
