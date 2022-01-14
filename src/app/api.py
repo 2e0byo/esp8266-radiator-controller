34c5aa60548a06d829c70edf071d21f074e4a571
John Maximilian <2e0byo@gmail.com>
2022-01-14 16:25:16 +0000

    feat(app): add next endpoint.

--- src/app/api.py
+++ src/app/api.py
@@ -1,9 +1,17 @@
 import log
 import tinyweb
 
+from . import radiator
+
+
 app = tinyweb.webserver(debug=True)
 
 app.add_resource(log.api, "/api/syslog/")
 
 
+@app.resource("/api/radiator/next/")
+async def next(data):
+    print("in next")
+    print(data)
+    return {"next-wakeup": radiator.scheduler.next_wakeup}
 app.run(port=80, loop_forever=False, host="0.0.0.0")
