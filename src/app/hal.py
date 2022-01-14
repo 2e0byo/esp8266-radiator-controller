c168ce6924f9f8c77f2e333bac052c1ab2de7ee7
John Maximilian <2e0byo@gmail.com>
2022-01-14 14:43:36 +0000

    refactor: dump old code.

--- src/app/hal.py
+++ src/app/hal.py
@@ -6,77 +6,6 @@ from math import pi, sin
 from machine import PWM, Pin
 
 
-class Radiator:
-    HEATING_ON = 0
-    HEATING_OFF = 1
-    WARMING_ON = 2
-    WARMING_OFF = 3
-
-    def __init__(self, relay):
-        self._relay = relay
-        relay.off()
-        self._warm_ms = 30 * 60 * 1000
-        self._timer = Delay_ms(self._stop_warm, self.delay)
-        self._timer.stop()
-        self._warming = False
-        self._heating = False
-        self._update = Message()
-
-    def start(self, loop):
-        loop.create_task(self._loop())
-
-    @property
-    def delay(self):
-        return self._warm_ms / 60_000
-
-    @delay.setter
-    def delay(self, val):
-        self._warm_ms = delay * 60_000
-        self._timer._durn = self._warm_ms
-
-    @property
-    def heating(self):
-        return self._heating
-
-    @heating.setter
-    def heating(self, val):
-        if self._heating is not val:
-            self._heating = val
-            state = self.HEATING_ON if val else self.HEATING_OFF
-            self._update.set(state)
-
-    @property
-    def warming(self):
-        return self._warming
-
-    @warming.setter
-    def warming(self, val):
-        if self._warming is not val:
-            self._warming = val
-            if val:
-                state = self.WARMING_ON
-                self._timer.trigger()
-            else:
-                state = self.WARMING_OFF
-                self._timer.stop()
-            self._update.set(state)
-
-    def _stop_warm(self):
-        self._update.set(self.WARMING_OFF)
-
-    async def _loop(self):
-        while True:
-            msg = await self._update.wait()
-            if msg is self.HEATING_ON:
-                self.relay.on()
-            elif msg is self.HEATING_OFF and not self._warming:
-                self.relay.off()
-            elif msg is self.WARMING_ON:
-                self.relay.on()
-            elif msg is self.WARMING_OFF and not self._heating:
-                self.relay_off()
-
-
 class Led:
     # led which can be flashed nicely
     RED = 0
@@ -127,6 +56,7 @@ class Buzzer:
         self._ringing = False
         self._update = Message()
         self.ring_freq = 600
+        asyncio.create_task(self._loop())
 
     def start(self, loop):
         loop.create_task(self._loop())
@@ -225,7 +155,3 @@ radiator = NoisyPin(0, Pin.OUT, led=led)
 
 _buzzer = PWM(Pin(12))
 buzzer = Buzzer(_buzzer)
-
-
-def init(loop):
-    buzzer.start(loop)
