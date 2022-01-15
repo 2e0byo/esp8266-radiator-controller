ded3159628be0d144be4859e125e2b9c87a45937
John Maximilian <2e0byo@gmail.com>
2022-01-15 18:36:48 +0000

    tests: path includes lib.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,5 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent.resolve()))
+sys.path.insert(0, str(Path(__file__).parent.parent.resolve() / "lib"))
 print(sys.path)
