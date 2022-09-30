5baa2be26e827019b438cd42a116bf3c6ac683c4
John Maximilian <2e0byo@gmail.com>
2022-09-30 11:17:30 +0100

    feat: Sensor() class and tests.

---
+++ lib/sensor/sensor.py
@@ -0,0 +1,102 @@
+import logging
+
+try:
+    import uasyncio as asyncio
+except ImportError:
+    import asyncio
+
+
+class Sensor:
+    """A sensor which is read periodically."""
+
+    def __init__(
+        self,
+        *args,
+        name,
+        log=True,
+        period=60,
+        moving_average=10,
+    ):
+        """Initialise a new Sensor()."""
+        self.name = name
+        self._logger = logging.getLogger(name) if log else None
+        self._val = []
+        self.period = period
+        self.moving_average = moving_average
+        self._dead = False
+        asyncio.get_event_loop().create_task(self._loop())
+
+    def _log(self, level, *args, **kwargs):
+        if self._logger:
+            getattr(self._logger, level)(*args, **kwargs)
+
+    def _dir(self):
+        """Sensor is dead."""
+        self._log("warning", f"Sensor {self.name} died.")
+        self._dead = True
+        self._val = []
+
+    @property
+    def value(self):
+        """Sensor value."""
+        if not self._val or self._dead:
+            return None
+        return sum(self._val) / len(self._val)
+
+    def _append_val(self, x):
+        nvals = len(self._val)
+        ma = self.moving_average
+        if nvals >= ma:
+            self._val = self._val[nvals - ma :]
+        self._val.append(x)
+
+    async def read_sensor(self):
+        """Read from sensor.
+
+        Children must implement this."""
+        raise NotImplementedError("Implement this")  # pragma: nocover
+
+    async def _loop(self):
+        silent_count = 0
+        while True:
+            try:
+                self._append_val(await self.read_sensor())
+                silent_count = 0
+            except Exception as e:
+                self._log("error", f"Read sensor failed: {str(e)}")
+                silent_count += 1
+                if silent_count > 10:
+                    self._dir()
+
+            await asyncio.sleep(self.period)
+
+
+class DummySensor(Sensor):
+    """Dummy implementation, for testing or experimentation."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialise a new DummySensor()."""
+        super().__init__(*args, **kwargs)
+        self._sensor_val = None
+        self._got = 0
+        self._set = []
+        self.sensor_vals = None
+
+    @property
+    def sensor_val(self):
+        """Get sensor value."""
+        self._got += 1
+        return self._sensor_val
+
+    @sensor_val.setter
+    def sensor_val(self, x):
+        """Set sensor value."""
+        self._set.append(x)
+        self._sensor_val = x
+
+    async def read_sensor(self):
+        """Read sensor"""
+        if not self.sensor_vals:
+            return self._sensor_val
+        else:
+            return next(self.sensor_vals)
---
+++ tests/test_sensor.py
@@ -0,0 +1,55 @@
+import asyncio
+
+import pytest
+from sensor.sensor import DummySensor
+
+
+async def sleep_ms(val):
+    await asyncio.sleep(val / 1_000)
+
+
+@pytest.fixture
+def sensor():
+    d = DummySensor(name="dummy", period=0.000001)
+    d.sensor_val = 0
+    return d
+
+
+async def test_val(sensor):
+    sensor.sensor_val = 100
+    await sleep_ms(1)
+    assert sensor.value == 100
+
+
+async def test_average(sensor):
+    sensor.sensor_vals = iter([55, 45] * 1_000)
+    await sleep_ms(5)
+    assert sensor.value == pytest.approx(50, 1)
+    sensor.sensor_vals = iter([50, 50, 50, 50, 55] * 1_000)
+    assert sensor.value == pytest.approx(50, 1)
+
+
+async def test_die(sensor, mocker):
+    read_sensor = mocker.AsyncMock()
+    read_sensor.side_effect = Exception()
+    mocker.patch.object(sensor, "read_sensor", read_sensor)
+    assert sensor.value == 0
+    await sleep_ms(10)
+    read_sensor.assert_awaited()
+    assert sensor.value is None
+    assert sensor.sensor_val == 0  # last set val
+
+
+async def test_die_no_log(mocker):
+    period = 0.000001
+    sensor = DummySensor(name="dummy", log=False, period=period)
+    sensor.sensor_val = 0
+    read_sensor = mocker.AsyncMock()
+    read_sensor.side_effect = Exception()
+    await asyncio.sleep(period)
+    assert sensor.value == 0
+    sensor.read_sensor = read_sensor
+    await sleep_ms(2)
+    read_sensor.assert_awaited()
+    assert sensor.value is None
+    assert sensor.sensor_val == 0  # last set val
