6c17ea451796e829298879b8089f8811398fabaa
John Maximilian <2e0byo@gmail.com>
2022-11-27 17:57:00 +0000

    fix: cleanup.

--- src/cleanup.py
+++ src/cleanup.py
@@ -5,12 +5,15 @@ DELETE = ("mpy", "py")
 
 
 def cleanup():
-    paths = "/", "/app/"
+    paths = "/", "/app/", "/static/"
     for path in paths:
-        for f in os.listdir(path):
-            suffix = f.split(".")[-1]
-            if suffix in DELETE and not any(x in f for x in PRESERVE):
-                os.remove("/".join((path, f)))
+        try:
+            for f in os.listdir(path):
+                suffix = f.split(".")[-1]
+                if suffix in DELETE and not any(x in f for x in PRESERVE):
+                    os.remove("/".join((path, f)))
+        except OSError:
+            pass
     for path in paths:
         try:
             os.rmdir(path)
