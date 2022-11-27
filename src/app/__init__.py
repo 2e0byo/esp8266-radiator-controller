c06b3cf02f333d14bf0624017df038fc3d06a431
John Maximilian <2e0byo@gmail.com>
2022-11-27 17:57:08 +0000

    refactor: make fn async.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -30,14 +30,14 @@ from . import api
 _step()
 
 
-def blink_hello():
+async def blink_hello():
     # say hello to the world
     for _ in range(3):
-        asyncio.run(hal.led.flash(hal.led.RED, 0.2))
-        asyncio.run(hal.led.flash(hal.led.GREEN, 0.2))
+        await hal.led.flash(hal.led.RED, 0.2)
+        await hal.led.flash(hal.led.GREEN, 0.2)
 
 
-blink_hello()
+asyncio.run(blink_hello())
 logger.info("Everything started")
 print(f"WDT validating the state of {len(wdt.state) - 1} processes.")
 
